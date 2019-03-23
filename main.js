1ab10eb6b1f08e330f9d8b7dd10e3662b596ea3a
Woody Maddox <woody.e.maddox@gmail.com>
2019-03-23 11:10:24 -0500

    styled cards

--- main.js
+++ main.js
@@ -1,64 +1,63 @@
 const products = [
-{
-    title: 'product1',
-    description: 'bla bla bla',
-    imageUrl: 'https://www.catster.com/wp-content/uploads/2018/05/A-gray-cat-crying-looking-upset.jpg',
-    size: '4" x 4"',
-    weight: '8 oz.',
-    price1: '$1.99',
-    price2: '$1.49',
-    price3: '$0.99',
-    validUntil: "04/02/2016",
+    {
+        title: 'SPACE-FOX',
+        description: 'Premium Space Fox',
+        imageUrl: 'https://techcrunch.com/wp-content/uploads/2018/07/logo-2.png?w=300',
+        Planet: 'Planet Zero',
+        weight: '8 oz.',
+        price1: '$1.99',
+        price2: '$1.49',
+        price3: '$0.99',
+        validUntil: "04/02/2016",
 
-},
-{
-    title: 'product2',
-    description: 'bla bla bla',
-    imageUrl: 'https://www.catster.com/wp-content/uploads/2018/05/A-gray-cat-crying-looking-upset.jpg',
-    size: '4" x 4"',
-    weight: '8 oz.',
-    price1: '$1.99',
-    price2: '$1.49',
-    price3: '$0.99',
-    validUntil: "04/02/2016",
+    },
+    {
+        title: 'SPACE-FOX',
+        description: 'Premium Space Fox',
+        imageUrl: 'https://techcrunch.com/wp-content/uploads/2018/07/logo-2.png?w=300',
+        Planet: 'Planet Zero',
+        weight: '8 oz.',
+        price1: '$1.99',
+        price2: '$1.49',
+        price3: '$0.99',
+        validUntil: "04/02/2016",
 
-},
-{
-    title: 'product3',
-    description: 'bla bla bla',
-    imageUrl: 'https://www.catster.com/wp-content/uploads/2018/05/A-gray-cat-crying-looking-upset.jpg',
-    size: '4" x 4"',
-    weight: '8 oz.',
-    price1: '$1.99',
-    price2: '$1.49',
-    price3: '$0.99',
-    validUntil: "04/02/2016",
+    },
+    {
+        title: 'SPACE-FOX',
+        description: 'Premium Space Fox',
+        imageUrl: 'https://techcrunch.com/wp-content/uploads/2018/07/logo-2.png?w=300',
+        Planet: 'Planet Zero',
+        weight: '8 oz.',
+        price1: '$1.99',
+        price2: '$1.49',
+        price3: '$0.99',
+        validUntil: "04/02/2016",
 
-},
-{
-    title: 'product4',
-    description: 'bla bla bla',
-    imageUrl: 'https://www.catster.com/wp-content/uploads/2018/05/A-gray-cat-crying-looking-upset.jpg',
-    size: '4" x 4"',
-    weight: '8 oz.',
-    price1: '$1.99',
-    price2: '$1.49',
-    price3: '$0.99',
-    validUntil: "04/02/2016",
+    },
+    {
+        title: 'SPACE-FOX',
+        description: 'Premium Space Fox',
+        imageUrl: 'https://techcrunch.com/wp-content/uploads/2018/07/logo-2.png?w=300',
+        Planet: 'Planet Zero',
+        weight: '8 oz.',
+        price1: '$1.99',
+        price2: '$1.49',
+        price3: '$0.99',
+        validUntil: "04/02/2016",
 
-},
-{
-    title: 'product5',
-    description: 'bla bla bla',
-    imageUrl: 'https://www.catster.com/wp-content/uploads/2018/05/A-gray-cat-crying-looking-upset.jpg',
-    size: '4" x 4"',
-    weight: '8 oz.',
-    price1: '$1.99',
-    price2: '$1.49',
-    price3: '$0.99',
-    validUntil: "04/02/2016",
-
-}
+    },
+    {
+        title: 'SPACE-FOX',
+        description: 'Premium Space Fox',
+        imageUrl: 'https://techcrunch.com/wp-content/uploads/2018/07/logo-2.png?w=300',
+        Planet: 'Planet Zero',
+        weight: '8 oz.',
+        price1: '$1.99',
+        price2: '$1.49',
+        price3: '$0.99',
+        validUntil: "04/02/2016",
+    },
 ];
 
 
@@ -67,25 +66,27 @@ const printToDom = (divId, textToprint) => {
     selectedDiv.innerHTML = textToprint;
 };
 
-    const createCards = () => {
-        let domString = '';
+const createCards = () => {
+    let domString = '';
 
-       products.forEach((cat) => {
-            domString += `<div class = "card">`;
-            domString += `  <div class="title"><h2>${cat.title}</h2></div>`;
-            domString += `  <div class="description"><h2>${cat.description}</h2></div>`;
-            domString += `  <div class="image"><img src=${cat.imageUrl}></div>`;
-            domString += `  <div class="color"><p>${cat.size}</p></div>`;
-            domString += `  <div class="Weight"><p>Weight: ${cat.weight}</p></div>`;
-            domString += `  <div class="Price1"><h2> ${cat.price1}</h2></div>`;
-            domString += `  <div class="Price2"><h2> ${cat.price2}</h2></div>`;
-            domString += `  <div class="Price3"><h2> ${cat.price3}</h2></div>`;
-            domString += `</div>`;
-            })
-            printToDom('main-content', domString);
-        }
-        const init = () => {
-        createCards();
+    products.forEach((cat) => {
+        domString += `  <div class="col-4">`;
+        domString += `    <div class="card">`;
+        domString += `      <h5 class="card-title">${cat.title}</h5>`;
+        domString += `      <div class="description">${cat.description}</div>`;
+        domString += `      <div class="image"><img src=${cat.imageUrl} style="width:70%"></div>`;
+        domString += `      <div class="color"><p>${cat.Planet}</p></div>`;
+        domString += `      <div class="Weight"><p>Weight: ${cat.weight}</p></div>`;
+        domString += `      <div class="Price1">${cat.price1}</div>`;
+        domString += `      <div class="Price2">${cat.price2}</div>`;
+        domString += `      <div class="Price3">${cat.price3}</div>`;
+        domString += `   </div>`;
+        domString += ` </div>`;
+    })
+    printToDom('main-content', domString);
+}
+const init = () => {
+    createCards();
 
-        };
-        init();
+};
+init();
